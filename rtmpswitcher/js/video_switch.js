f38c78546d2ae6e7694894c38d922681cdd16fe8
taras-ledkov <taras.ledkov@gmail.com>
2012-07-20 03:18:01 +0400

    Features: video switch show selected channel, create task changes

--- rtmpswitcher/js/video_switch.js
+++ rtmpswitcher/js/video_switch.js
@@ -1,9 +1,14 @@
-function video_switch(url_switch)
+function video_switch(url_switch, chid)
 {
-//    document.getElementById("ch-2").style.backgroundColor = "red";
+    jQuery('#channels-table').find('tr').each(function(i, el) {
+        jQuery(el).css({
+            backgroundColor : ''
+        });
+    });
     
+    jQuery('#'+chid).css({
+        backgroundColor : 'lightblue'
+    });
+
     jQuery.get(url_switch);
-//    jQuery.get(url_switch, function(data) {
-//        alert('Сервер ответил: ' + data);
-//    });
 }
