d398697b7a0d33911e5ab62d2abac7717268e06d
taras-ledkov <taras.ledkov@gmail.com>
2012-07-19 05:13:53 +0400

    Features: config, video switch (initial)

---
+++ rtmpswitcher/js/video_switch.js
@@ -0,0 +1,9 @@
+function video_switch(url_switch)
+{
+//    document.getElementById("ch-2").style.backgroundColor = "red";
+    
+    jQuery.get(url_switch);
+//    jQuery.get(url_switch, function(data) {
+//        alert('Сервер ответил: ' + data);
+//    });
+}
